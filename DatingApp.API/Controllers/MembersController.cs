b2c112c258dfc8ed285f9fa2a0ca0f841147a6d3
habee070 <habee070@gmail.com>
2026-07-08 06:09:36 +0700

    Add project files.

---
+++ DatingApp.API/Controllers/MembersController.cs
@@ -0,0 +1,26 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers;
+
+[Route("api/[controller]")] // locahost:5000/api/members
+[ApiController]
+public class MembersController(AppDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        var members = await context.Users.ToListAsync();
+        return members;
+    }
+
+    [HttpGet("{id}")] // locahost:5000/api/members/bob-id
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await context.Users.FindAsync(id);
+        if (member == null) return NotFound();
+        return member;
+    }
+}
