242c9e227278e7558490e99453be053f620286e6
habee070 <habee070@gmail.com>
2026-08-07 20:59:35 +0700

    End of section 9

---
+++ client/src/types/Member.ts
@@ -0,0 +1,19 @@
+export type Member = {
+  id: string;
+  dateOfBirth: string;
+  imageUrl?: string;
+  created: string;
+  lastActive: string;
+  displayName: string;
+  gender: string;
+  description?: string;
+  city: string;
+  country: string;
+};
+
+export type Photo = {
+  id: number;
+  url: string;
+  publicId?: any;
+  memberId: string;
+};
